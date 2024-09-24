9e4b799781a081a161ae74c4eb9e0a86e95cc3e4
EC2 Default User <ec2-user@ip-172-31-19-217.ec2.internal>
2024-09-24 07:20:19 +0000

    segunda entrega

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database.db import connect2SQL, add_user
 
 app = Flask(__name__)
 
-@app.route('/register_user')
-def register():
+@app.route('/register_page')
+def register_page():
     return render_template("register.html")
+
+@app.route('/register_user', methods = ["post"])
+def register_user():
+    data_user = request.form
+    id = data_user["id"]
+    last_name = data_user["last_name"]
+    first_name = data_user["first_name"]
+    birthday = data_user["birthday"]
+    add_user(id,first_name,last_name,birthday)
+    return "User added"
+
 
 if __name__ == "__main__":
     ip = "172.31.19.217"
